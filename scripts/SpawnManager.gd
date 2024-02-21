3d6c1aa70e4ea058af0cd4ec5091491b16b6eb19
Tekknobot <thrillatron@hotmail.com>
2024-02-20 23:17:16 -0500

    Add humans.

--- scripts/SpawnManager.gd
+++ scripts/SpawnManager.gd
@@ -10,8 +10,10 @@ var USER_units = []
 var open_tiles = []
 var random = []
 
-var zombie = preload("res://scenes/sprites/zombie.scn")
-var dog = preload("res://scenes/sprites/dog.scn")
+var zombie = preload("res://scenes/sprites/Zombie.scn")
+var dog = preload("res://scenes/sprites/Dog.scn")
+var soldier = preload("res://scenes/sprites/Soldier.scn")
+var rambo = preload("res://scenes/sprites/Rambo.scn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -65,7 +67,51 @@ func spawn():
 		var tween: Tween = create_tween()
 		tween.tween_property(dog_inst, "position", new_position, 1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)	
 		get_node("../TileMap").astar_grid.set_point_solid(new_position, true)
-		await get_tree().create_timer(0).timeout
+		await get_tree().create_timer(0.5).timeout
+
+	# Find open tiles again
+	open_tiles.clear()	
+	for i in 16:
+		for j in 16:
+			if get_node("../TileMap").astar_grid.is_point_solid(Vector2i(i,j)) == false:			
+				open_tiles.append(Vector2i(i,j))
+	
+	random.clear()
+	random = get_random_numbers(0, open_tiles.size())
+
+	# Drop soldier at start	
+	for i in 1:	
+		var soldier_inst = soldier.instantiate()
+		node2D.add_child(soldier_inst)
+		soldier_inst.add_to_group("humans")			
+		var new_position = get_node("../TileMap").map_to_local(open_tiles[random[i]]) + Vector2(0,0) / 2
+		soldier_inst.position = Vector2(new_position.x, new_position.y-500)
+		var tween: Tween = create_tween()
+		tween.tween_property(soldier_inst, "position", new_position, 1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)	
+		get_node("../TileMap").astar_grid.set_point_solid(new_position, true)
+		await get_tree().create_timer(0.5).timeout
+
+	# Find open tiles again
+	open_tiles.clear()	
+	for i in 16:
+		for j in 16:
+			if get_node("../TileMap").astar_grid.is_point_solid(Vector2i(i,j)) == false:			
+				open_tiles.append(Vector2i(i,j))
+	
+	random.clear()
+	random = get_random_numbers(0, open_tiles.size())
+
+	# Drop Rambo at start	
+	for i in 1:	
+		var rambo_inst = rambo.instantiate()
+		node2D.add_child(rambo_inst)
+		rambo_inst.add_to_group("humans")			
+		var new_position = get_node("../TileMap").map_to_local(open_tiles[random[i]]) + Vector2(0,0) / 2
+		rambo_inst.position = Vector2(new_position.x, new_position.y-500)
+		var tween: Tween = create_tween()
+		tween.tween_property(rambo_inst, "position", new_position, 1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)	
+		get_node("../TileMap").astar_grid.set_point_solid(new_position, true)
+		await get_tree().create_timer(0.5).timeout
 	
 	await get_tree().create_timer(1).timeout	
 	
@@ -79,4 +125,4 @@ func get_random_numbers(from, to):
 
 func _on_spawn_button_pressed():
 	await spawn()
-	get_node("../TileMap").dog_attack_ai()
+	#get_node("../TileMap").dog_attack_ai()
--- scripts/TileMapManager.gd
+++ scripts/TileMapManager.gd
@@ -12,6 +12,7 @@ var rng = RandomNumberGenerator.new()
 var astar_grid = AStarGrid2D.new()
 var zombies = []
 var dogs = []
+var humans = []
 
 var moved = false
 
@@ -83,11 +84,64 @@ func dog_attack_ai():
 			closest_atack.add_to_group("dead")
 			closest_atack.remove_from_group("zombies")
 			
-			if moved == false:
-				dog_attack_ai()
-			if moved == true:
-				pass
 				
+func humans_attack_ai():
+	zombies = get_tree().get_nodes_in_group("zombies")
+	humans = get_tree().get_nodes_in_group("humans")
+	var active_humans = rng.randi_range(0,humans.size()-1)
+	var target_zombie = rng.randi_range(0,zombies.size()-1)			
+	if !zombies[target_zombie].is_in_group("dead"):
+		var closest_atack = humans[active_humans].get_closest_attack_zombies()										
+		var zombie_target_pos = local_to_map(closest_atack.position)
+		var zombie_surrounding_cells = get_surrounding_cells(zombie_target_pos)
+		
+		humans[active_humans].get_child(0).play("move")
+		var open_tile = rng.randi_range(0,3)
+		if astar_grid.is_point_solid(zombie_surrounding_cells[open_tile]) == false and get_cell_source_id(0, zombie_surrounding_cells[open_tile]) != -1: 
+			var patharray = astar_grid.get_point_path(humans[active_humans].tile_pos, zombie_surrounding_cells[open_tile])
+			# Find path and set hover cells
+			for h in patharray.size():
+				await get_tree().create_timer(0.01).timeout
+				set_cell(1, patharray[h], 10, Vector2i(0, 0), 0)
+			# Move unit		
+			for h in patharray.size():
+				var tile_center_position = map_to_local(patharray[h]) + Vector2(0,0) / 2
+				var tween = create_tween()
+				tween.tween_property(humans[active_humans], "position", tile_center_position, 0.25)
+				var unit_pos = local_to_map(humans[active_humans].position)
+				humans[active_humans].z_index = unit_pos.x + unit_pos.y			
+				await get_tree().create_timer(0.25).timeout
+				moved = true
+			
+			# Remove hover cells
+			for h in patharray.size():
+				set_cell(1, patharray[h], -1, Vector2i(0, 0), 0)
+				
+			humans[active_humans].get_child(0).play("default")	
+			
+			for i in 4:
+				var zombies_pos = local_to_map(humans[active_humans].position)
+				if zombies_pos == zombie_surrounding_cells[i]:
+					var attack_center_pos = map_to_local(zombie_target_pos) + Vector2(0,0) / 2
+								
+					if humans[active_humans].scale.x == 1 and humans[active_humans].position.x > attack_center_pos.x:
+						humans[active_humans].scale.x = 1
+					elif humans[active_humans].scale.x == -1 and humans[active_humans].position.x < attack_center_pos.x:
+						humans[active_humans].scale.x = -1	
+					if humans[active_humans].scale.x == -1 and humans[active_humans].position.x > attack_center_pos.x:
+						humans[active_humans].scale.x = 1
+					elif humans[active_humans].scale.x == 1 and humans[active_humans].position.x < attack_center_pos.x:
+						humans[active_humans].scale.x = -1						
+		
+			humans[active_humans].get_child(0).play("attack")
+			var tween: Tween = create_tween()
+			tween.tween_property(closest_atack, "modulate:v", 1, 0.50).from(5)			
+			await get_tree().create_timer(1).timeout
+			closest_atack.get_child(0).play("death")
+			humans[active_humans].get_child(0).play("default")	
+			closest_atack.add_to_group("dead")
+			closest_atack.remove_from_group("zombies")
+			
 			
 func zombie_attack_ai():
 	zombies = get_tree().get_nodes_in_group("zombies")
@@ -150,3 +204,6 @@ func _on_zombie_button_pressed():
 
 func _on_dog_button_pressed():
 	dog_attack_ai()
+
+func _on_soldier_button_pressed():
+	humans_attack_ai()
