6b33c44ed381b3c5c8bb66a8a82f4b8db5a7dc4d
Tekknobot <thrillatron@hotmail.com>
2024-02-27 17:26:15 -0500

    Tidy up refactor.

--- scripts/ExplosionManager.gd
+++ scripts/ExplosionManager.gd
@@ -5,7 +5,6 @@ extends Area2D
 func _ready():
 	get_node("../TileMap").moving = false
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- scripts/ProjectileManager.gd
+++ scripts/ProjectileManager.gd
@@ -5,7 +5,6 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var tile_pos = get_node("../TileMap").local_to_map(self.position)
