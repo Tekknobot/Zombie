6d0ce2c01de313a1017586309ed1a2b6fe0b1c78
Tekknobot <thrillatron@hotmail.com>
2024-03-14 11:06:26 -0400

    Add remaining sprite portrait and animations.

--- scripts/PortraitsManager.gd
+++ scripts/PortraitsManager.gd
@@ -6,11 +6,11 @@ var rng = RandomNumberGenerator.new()
 @onready var Dialogue : = $Dialogue
 @onready var Map : = $"../../TileMap"
 @onready var Hovertile : = $"../../Hovertile"
-
+	
 var soldier = preload("res://assets/portraits/soldier_port.png")
 var zombie = preload("res://assets/portraits/zombie_port.png")
 var dog = preload("res://assets/portraits/dog_port.png")
-var rambo = preload("res://assets/portraits/Zombies/Rambo_portrait.png")
+var rambo = preload("res://assets/portraits/rambo_port.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,11 +31,15 @@ func _input(event):
 				var tile_center_pos = Map.map_to_local(tile_pos) + Vector2(0,0) / 2
 				
 				for i in Map.all_units.size():				
-					if Map.all_units[i].unit_type == "Zombie" and Map.all_units[i].tile_pos == tile_pos:	
+					if Map.all_units[i].unit_type == "Zombie" and Map.all_units[i].tile_pos == tile_pos:
+						self.show()	
 						Portrait.texture = zombie
 					if Map.all_units[i].unit_name == "Butch" and Map.all_units[i].tile_pos == tile_pos:	
+						self.show()	
 						Portrait.texture = soldier
 					if Map.all_units[i].unit_type == "Dog" and Map.all_units[i].tile_pos == tile_pos:	
+						self.show()	
 						Portrait.texture = dog		
 					if Map.all_units[i].unit_name == "Snake" and Map.all_units[i].tile_pos == tile_pos:	
+						self.show()	
 						Portrait.texture = rambo		
